7ff82a2af19bc9eedd59d2eefa41ecb6c325f0e6
pabloacisera <pabloacisera2021@gmail.com>
2024-01-23 00:06:12 -0300

    25jan24

---
+++ exercise.js
@@ -0,0 +1,15 @@
+let listaAlumnos = {
+    1: 'Juan',
+    2: 'Pedro',
+    3: 'Sorete'
+};
+
+console.log(listaAlumnos[3]);
+
+let autosVendidos = {
+    "chevrolet": 566,
+    "ford": 528,
+    "audi": 2
+};
+
+console.log(autosVendidos.chevrolet);
